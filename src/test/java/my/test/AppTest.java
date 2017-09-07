ee4320fcddd4b88d75e062b82cb665e35b9777a0
Yuki INOUE <inoueyuworks@gmail.com>
2017-09-07 15:40:45 +0900

    test maven wagon

---
+++ src/test/java/my/test/AppTest.java
@@ -0,0 +1,15 @@
+package my.test;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Category(SlowTest.class)
+    @Test
+    public void aTest() {
+    }
+}
